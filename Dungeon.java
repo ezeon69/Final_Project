af17fc233824a33181d2b6857818e0bf164a5cf9
Jiajun Luo <luo.jiajun.jiajun@gmail.com>
2017-05-31 14:48:13 -0400

    start site, dungeon

---
+++ Dungeon.java
@@ -0,0 +1,24 @@
+public class Dungeon{
+    
+    private int size;
+
+    
+    
+    
+    public int size(){
+	return size;
+    }
+    
+    public boolean isLegalMove(Site a, Site b){
+	
+    }
+    
+    public boolean isCorridor(Site s){
+        
+    }
+
+    
+    
+    
+    
+}
--- Site.java
+++ Site.java
@@ -1,3 +1,26 @@
 public class Site{
 
+    public Site s;
+    private int row;
+    private int col;
+
+    public Site(int r,int c){
+	row = r;
+	col = c;
+    }
+
+    public int getRow(){
+	return row;
+    }
+
+    public int getCol(){
+	return col;
+    }
+
+    public boolean equals(Site Other){
+	return (s.getRow() == Other.getRow()) && (s.getCol() == Other.getCol());
+    }
+    
+    
+
 }
